cce1e2c00e3506d690a113c9e722bf79a58e6a11
xFireBerg <32838402+xFireBerg@users.noreply.github.com>
2020-08-13 20:08:31 +0300

    added solution to variable swap

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PIWorks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give a number as 1st variable: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Please give a number as 2nd variable: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+                        
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine("The value of the 1st variable: " + a);
+            Console.WriteLine("The value of the 2nd variable: " + b);
+
+            Console.WriteLine("\nPress any key to exit");
+            Console.ReadKey(true);
+        }
+
+    }
+}
